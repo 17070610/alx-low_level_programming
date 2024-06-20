e89c8fcb7e558d1085d94f2dfb68e91e829d6779
root <schimea@hotmail.com>
2024-06-20 03:57:42 -0700

    Use puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints this "Programming is like building a multilingual puzzle"
- * Return: 0 if exited properly, non-zero otherwise  */
-int main(void) {
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
