296fe7dc1d0c8d590f9a1f3dd32ee3b643c50e0c
root <schimea@hotmail.com>
2024-06-20 05:17:43 -0700

    Use printf instead of puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - prints this "with proper grammar, but the outcome is a piece of art,"
  * Return: 0 if exited properly, if not then a non-zero
  */
-int main () 
+int main(void) 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
