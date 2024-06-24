96d9e81465da38b364e9e962aa933c10ec244b31
root <schimea@hotmail.com>
2024-06-24 05:27:58 -0700

    Print all double digit numbers starting from 0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all double digits numbers starting from 0.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int tens;
+	int units;
+
+	for (tens = 0; tens < 10; tens++)
+	{
+		for (units = 0; units < 10; units++)
+		{
+			putchar(tens + '0');
+			putchar(units + '0');
+			if (tens != 9 && units != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
