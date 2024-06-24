b2c9924ab7ec282ca17edc94dbc65f99b4b0c89c
root <schimea@hotmail.com>
2024-06-24 01:52:01 -0700

    Print all numbers of hexadecimal base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of hexadecimal base.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		}
+		else
+		{
+			putchar(num - 10 + 'a');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
