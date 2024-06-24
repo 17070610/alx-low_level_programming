0a9674418210c08b760ac4eb0eb4313d6063d6c0
root <schimea@hotmail.com>
2024-06-24 02:10:30 -0700

    Print all single digit numbers separated by a comma and a space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations of single-digit numbers.
+ * Return: Alwaays 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0')
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
