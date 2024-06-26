8d4c3874af6f283744d2aa2f76aedc3c45cf3bbb
root <schimea@hotmail.com>
2024-06-26 01:18:53 -0700

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter a; a <= z; a++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
