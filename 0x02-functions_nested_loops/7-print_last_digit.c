3b4664d04711304fa7e57271c3ccff72ce0e56a8
root <schimea@hotmail.com>
2024-06-26 04:55:04 -0700

    Return the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of a number
+ * @i: The character that is going to be checked
+ * Return: The last digit of the number
+ */
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
