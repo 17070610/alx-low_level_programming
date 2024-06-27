fe55305bc0fe1cd949603f02488a30521b942297
root <schimea@hotmail.com>
2024-06-27 04:52:19 -0700

    Print numbers for 0 to 9 except few

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9 except few
+ */
+
+void print_most_numbers(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		if (numbers = 2 || numbers = 4)
+			continue;
+		else
+			_putchar(numbers + '0');
+	}
+	_putchar('\n');
+}
