c5f3e3b2951ad834a49eeea0c80399366f5fc587
root <schimea@hotmail.com>
2024-06-27 04:29:42 -0700

    prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - Prints numbers from 0 to 9
- * Return: Always 0
  */
 
 void print_numbers(void)
@@ -13,5 +12,4 @@ void print_numbers(void)
 		_putchar(numbers + '0');
 
 	_putchar('\n');
-	return (0);
 }
