94e401e74a6c5c8f0572748bdc3695611c1b0f46
root <schimea@hotmail.com>
2024-06-27 04:25:51 -0700

    prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; number++)
+		_putchar(numbers + '0');
+
+	_putchar('\n');
+	return (0);
+}
