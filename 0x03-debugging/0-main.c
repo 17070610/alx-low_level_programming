774cb184350e3b7e208694fd202d033169672390
root <schimea@hotmail.com>
2024-06-27 00:12:56 -0700

    Test a function that prints if int is + or -

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	positive_or_negative(-98);
+
+	return (0);
+}
