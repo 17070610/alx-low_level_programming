886fd895c02890d4880288d672ec0b6f9d027e11
root <schimea@hotmail.com>
2024-06-28 00:05:02 -0700

    Print numbers from 0 to 14 on 10 lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 
 	for (line = 0; line <= 10; line++)
 	{
-		for (num = 0; num <= 14;)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 10)
 				_putchar(num / 10 + '0');
