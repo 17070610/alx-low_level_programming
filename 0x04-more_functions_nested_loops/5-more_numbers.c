fa2c011ca742e3630db688bd7225046c6e52cc06
root <schimea@hotmail.com>
2024-06-28 00:00:00 -0700

    Print numbers from 0 to 14 on 10 lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 
+ * ten times
+ */
+
+void more_numbers(void)
+{
+	int line, num;
+
+	for (line = 0; line <= 10; line++)
+	{
+		for (num = 0; num <= 14;)
+		{
+			if (num >= 10)
+				_putchar(num / 10 + '0');
+			_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
