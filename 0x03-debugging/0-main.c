ddc2cf061f1f7c7792917752957d0a71ae515577
root <schimea@hotmail.com>
2024-06-27 22:46:09 -0700

    Remove errors

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function that prints 
+ * main - tests function that prints
  * if integer is positive or negative
  * Return: Always 0
  */
 
 int main(void)
 {
-	positive_or_negative(0);
+	int i = 0;
+
+	positive_or_negative(i);
 
 	return (0);
 }
