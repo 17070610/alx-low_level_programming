b91d6ef7cec508d397b9c4605dc865e00e30b5f8
root <schimea@hotmail.com>
2024-07-02 07:27:37 -0700

    Remove errors

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 
 /**
  * _strlen - returns the length of a string
- * Return - The length of the string
+ * Return: The length of the string
  * @s: is the string being checked
  */
 
 int _strlen(char *s)
 {
 	const char *str = s;
+
 	while (*str)
 	{
 		str++;
