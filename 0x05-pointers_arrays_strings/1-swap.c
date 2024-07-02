74d02b2ec65a379e88a26ab1c5348c304dc7f06a
root <schimea@hotmail.com>
2024-07-02 06:31:40 -0700

    Swap values of two intergers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two intergers
+ * @*a: The pointer passed in
+ * @*b: The pointer passed in
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
