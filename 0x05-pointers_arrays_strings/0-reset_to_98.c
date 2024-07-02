23d96fc06b42661cc32a5e6595e7edbeaa0585aa
root <schimea@hotmail.com>
2024-07-02 07:24:16 -0700

    Remove the errors

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - Takes a pointer to an int as a parameter and
+ * reset_to_98 - Takes a pointer to an int as a parameter and
  * updates the value it points to to 98
  * @n: The pointer to the int being changed
  */
