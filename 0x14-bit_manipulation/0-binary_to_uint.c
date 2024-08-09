1a293059a87966750570ac59efca5aa4a1594a62
root <schimea@hotmail.com>
2024-08-08 23:34:02 -0700

    Convert a binary string into an unsigned int in decimal base

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal;
+	unsigned int total, power;
+	int len;
 
 	if (b == NULL)
 		return (0);
 
-	while (*b)
+	for (len = 0; b[len]; len++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-
-		decimal = (decimal << 1) + (*b - '0');
-		b++;
 	}
 
-	return (decimal);
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
 }
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: The integer to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+
+}
