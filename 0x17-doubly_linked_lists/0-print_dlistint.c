35c34519c7d4abe453c170aa2299bc1968090432
17070610 <schimea@hotmail.com>
2024-09-19 03:51:21 -0700

    Prints all the elements of a list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_dlistint - Prints all elements of dlistint_t
+ * @h: head of the list
+ * Return: The number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t temp = h;
+
+	while(temp != NULL) {
+		count++
+		printf("%d", temp->n);
+		temp = temp->next
+	}
+	printf("\n");
+	return count
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# 0x17. C - Doubly linked lists
+
+## Description 
+
+This directory talks much about singly linked lists in c
